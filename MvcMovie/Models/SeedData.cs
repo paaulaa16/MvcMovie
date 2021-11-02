bf9c84e0f3b8f0b6c87f757ab396328d73608127
paaulaa16 <paaulacaballero@gmail.com>
2021-11-02 11:39:05 +0100

    paso8

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -26,7 +26,10 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
+                        Rating = "9",
                         Price = 7.99M
+                        
+
                     },
 
                     new Movie
@@ -34,6 +37,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
+                        Rating = "9",
                         Price = 8.99M
                     },
 
@@ -42,6 +46,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
+                        Rating = "9",
                         Price = 9.99M
                     },
 
@@ -50,6 +55,7 @@ namespace MvcMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
+                        Rating = "9",
                         Price = 3.99M
                     }
                 );
